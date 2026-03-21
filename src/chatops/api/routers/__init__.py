67533cf1294580b501f7a31087bc34775073a4f0
jaeminjo732 <jaeminjo732@gmail.com>
2026-03-21 20:36:02 +0900

    feat(api): stream 라우터 등록

--- src/chatops/api/routers/__init__.py
+++ src/chatops/api/routers/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from chatops.api.routers.health import router as health_router
 from chatops.api.routers.requests import router as request_router
 from chatops.api.routers.sessions import router as session_router
+from chatops.api.routers.stream import router as stream_router
 
 
 def build_api_router() -> APIRouter:
@@ -10,4 +11,5 @@ def build_api_router() -> APIRouter:
     router.include_router(health_router)
     router.include_router(session_router)
     router.include_router(request_router)
+    router.include_router(stream_router)
     return router
